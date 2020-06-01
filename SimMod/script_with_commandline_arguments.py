280ae41afda7c53cbba0ad010402bea7f9917c21
hvutuyen <24275944+hvutuyen@users.noreply.github.com>
2020-06-02 00:51:15 +0200

    file upload

---
+++ SimMod/script_with_commandline_arguments.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16 12:14:58 2016
+
+@author: toenjes
+"""
+import sys
+print("Liste der Agumente :",sys.argv)
---
+++ SimMod/while_example.py
@@ -0,0 +1,7 @@
+name = ''
+while name != "your name":
+    print("please type your name.")
+    name = input()
+print("Thank You, Bitch!")
+
+# solange nicht "your name" eingegeben wird, wiederholt sich der loop "please type your name"
